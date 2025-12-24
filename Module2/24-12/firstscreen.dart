398cafc57896c1dc9df980727744d6c5fa8968ae
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2025-12-24 16:55:57 +0530

    commit

---
+++ Module2/24-12/firstscreen.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+class FirstScreen extends StatefulWidget
+{
+  const FirstScreen({super.key});
+
+  @override
+  State<FirstScreen> createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen>
+{
+    TextEditingController uname = TextEditingController();
+    TextEditingController pass = TextEditingController();
+    var _formkey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context)
+  {
+    return Scaffold
+      (
+        appBar: AppBar(title: Text("Form Example",style: TextStyle(color: Colors.white),),backgroundColor: Colors.blueGrey,),
+        body: Form(
+          key: _formkey,
+          child: Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: Center
+              (
+                child: Column
+                  (
+                  children:
+                  [
+                      TextFormField(controller:uname,decoration: InputDecoration(hintText: "Enter Username"),
+                        validator: (value)
+                       {
+                         if (value!.isEmpty) {
+                           return 'Please Enter Username';
+                         }
+                         return null;
+                       },),
+                      SizedBox(height:15),
+                      TextFormField(controller:pass,decoration: InputDecoration(hintText: "Enter Password"),obscureText: true, validator: (value)
+                      {
+                        if (value!.isEmpty) {
+                          return 'Please Enter Password';
+                        }
+                        return null;
+                      },),
+                      SizedBox(height:15),
+                      ElevatedButton(onPressed: ()
+                      {
+                        if(_formkey.currentState!.validate())
+                        {
+                          String myuname = uname.text.toString();
+                          String mypass = pass.text.toString();
+
+                          if(myuname=="tops" && mypass=="1234")
+                          {
+
+                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Login Success")));
+                            //print("Login Success");
+                          }
+                          else
+                          {
+                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Login Fail")));
+                            //print("Login Fail");
+                          }
+                        }
+
+
+
+                          //print("$myuname and $mypass");
+
+                      }, child: Text("Login"))
+
+                  ],
+                )
+              ),
+          ),
+        ),
+      );
+  }
+}
---
+++ Module2/24-12/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'firstscreen.dart';
+
+void main()
+{
+  runApp(MaterialApp(home:FirstScreen(),debugShowCheckedModeBanner: false,));
+}
+
+
