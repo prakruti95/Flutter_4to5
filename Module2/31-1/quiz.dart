f63bc6afa0ec7d020c02daf8b5bd68fd2bfb6667
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2026-01-13 17:12:53 +0530

    commit

---
+++ Module2/31-1/quiz.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:quiz1/questions.dart';
+import 'package:quiz1/answer.dart';
+
+
+
+class Quiz extends StatelessWidget {
+  final List<Map<String, Object>> questions;
+  final int questionIndex;
+  final Function answerQuestion;
+
+  const Quiz({
+    Key? key,
+    required this.questions,
+    required this.answerQuestion,
+    required this.questionIndex,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Question(
+          questions[questionIndex]['questionText'].toString(),
+        ), //Question
+        ...(questions[questionIndex]['answers'] as List<Map<String, Object>>)
+            .map((answer) {
+          return Answer(selectHandler: () => answerQuestion(answer['score']), answerText: answer['text'].toString());
+        }).toList()
+      ],
+    ); //Column
+  }
+}
